a63b8edd00bda58c9ad78c135c2bae1e2d24435d
Pedro Ricardo C. Souza <pricardo@aimirimsti.com.br>
2023-02-15 16:56:57 -0300

    feat(DataPointCard): Change DataPointCard to use the new redux.

--- src/component/DataPointCard/DataPointCard.js
+++ src/component/DataPointCard/DataPointCard.js
@@ -5,17 +5,23 @@
  * 
  * Dependencies are:
  * - react 
+ * - react-redux
+ * - @mui/material
 */
 
 // Imports from modules;
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
 import {TableRow, Checkbox} from '@mui/material'
 // Local Imports
 import TitledCard from '../TitledCard/TitledCard';
 import DataTable from '../DataTable/DataTable';
 import TextCell from '../DataTable/TextCell';
 import CheckCell from '../DataTable/CheckCell';
+import { getDataPointAddress } from '../Protocols/Protocols';
+import * as popupsActions from '../../store/popups/actions';
+import * as datapointActions from '../../store/datapoint/actions';
 
 // #######################################
 
@@ -26,40 +32,65 @@ class DataPointCard extends React.PureComponent {
     
     /** Defines the component property types */
     static propTypes = {
-        content_rows:PropTypes.arrayOf(PropTypes.object),
-        selected_row:PropTypes.object,
-        onRowClick:PropTypes.func,
-        onNewClick:PropTypes.func,
-        onEditClick:PropTypes.func,
-        onDeleteClick:PropTypes.func,
     };
     
+    /** Defines the component state variables */
+    state = {
+        selected_row:{name:null,id:-1}
+    }
+
+    /** Description.
+    * @param ``: */
+    handleRowClick=(name,index) => {
+        const newState = {...this.state};
+        if (name===this.state.selected_row.name){
+            newState.selected_row = {name:null,id:-1};
+        } else {
+            newState.selected_row = {name:name,id:index};
+        }
+        this.setState(newState);
+    }
+
+    /** Description.
+    * @param ``: */
+    handleNewClick=() => {
+        this.props.onOpenPopup(true);
+        const newState = {...this.state};
+        this.setState(newState);
+    }
+    /** Description.
+    * @param ``: */
+    handleEditClick=() => {
+        const newState = {...this.state};
+        this.setState(newState);
+    }
+    /** Description.
+    * @param ``: */
+    handleDeleteClick=() => {
+        const newState = {...this.state};
+        this.setState(newState);
+    }
     /** Description.
     * @param ``: 
     * @returns */
-    getProtocolAddress=(row, prot_name) => {
-        let address;
-        if (prot_name==="Rockwell"){
-            address = row.access.data.tag_name;
-        } else {
-            address = row.access.data.address;
-        }
-        return(address)
+    handlePopUpLeave=() => {
+        this.props.onGetDataPoint(this.props.global.backend_instance);
+        this.props.onOpenPopup(false);
     }
 
     /** Description.
     * @param ``: 
     * @returns */
      buildContentRow=(row, index) => {
-        const is_selected = row.name===this.props.selected_row.name;
+        const is_selected = row.name===this.state.selected_row.name;
         const content = <TableRow hover
-            onClick={this.props.onRowClick.bind(this,row.name,index)}
+            onClick={this.handleRowClick.bind(this,row.name,index)}
             role="checkbox"
             tabIndex={-1}
             key={index}
             selected={is_selected} >
                 <CheckCell check_component={<Checkbox color="primary" checked={is_selected}/>}/>
-                {[row.name, row.description, this.getProtocolAddress(row,row.access.name), row.datasource_name].map(
+                {[row.name, row.description, getDataPointAddress(row,row.access.name), row.datasource_name].map(
                     (text, index) => <TextCell text={text} key={index}/>
                 )}
         </TableRow>
@@ -69,17 +100,18 @@ class DataPointCard extends React.PureComponent {
     /** Defines the component visualization.
     * @returns JSX syntax element */
     render(){
+        // console.info(this.props.datapoint.dp_content)
         const card_contents=[
             <DataTable
                 headers={["Name","Description","Address","Data Souce"]}
                 ncols_to_actions={2}
-                content_rows={this.props.content_rows}
-                selected_row={this.props.selected_row}
+                content_rows={this.props.datapoint.dp_content}
+                selected_row={this.state.selected_row}
                 buildContentRow={this.buildContentRow}
-                onRowClick={this.props.onRowClick}
-                onNewClick={this.props.onNewClick}
-                onEditClick={this.props.onEditClick}
-                onDeleteClick={this.props.onDeleteClick}/>,
+                onRowClick={this.handleRowClick}
+                onNewClick={this.handleNewClick}
+                onEditClick={this.handleEditClick}
+                onDeleteClick={this.handleDeleteClick}/>,
             ];
 
         const jsx_component = (
@@ -88,8 +120,25 @@ class DataPointCard extends React.PureComponent {
         );
         return(jsx_component);
     };
+
+    componentDidMount() {
+        this.props.onGetDataPoint(this.props.global.backend_instance)
+    };
     
 }
 
+/** Map the Redux state to some component props */
+const reduxStateToProps = (state) =>({
+    global: state.global,
+    popups: state.popups,
+    datapoint: state.datapoint
+});
+
+/** Map the Redux actions dispatch to some component props */
+const reduxDispatchToProps = (dispatch) =>({
+    onOpenPopup:(open)=>dispatch(popupsActions.openDataPointPopup(open)),
+    onGetDataPoint:(api)=>dispatch(datapointActions.getData(api)),
+});
+
 // Make this component visible on import
-export default DataPointCard;
+export default connect(reduxStateToProps,reduxDispatchToProps)(DataPointCard);
