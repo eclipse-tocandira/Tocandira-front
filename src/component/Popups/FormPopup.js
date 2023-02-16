0c79eb4cc4ab868fdbed2f087d2c3f39fb5cd776
Pedro Ricardo C. Souza <pricardo@aimirimsti.com.br>
2023-02-16 13:47:48 -0300

    fix(FormPopup): Align buttons with content and export maxWidth of card.

--- src/component/Popups/FormPopup.js
+++ src/component/Popups/FormPopup.js
@@ -39,28 +39,33 @@ class FormPopup extends React.PureComponent {
     static propTypes = {
         open:PropTypes.bool,
         title:PropTypes.string,
+        cardWidth:PropTypes.string,
         nameOk:PropTypes.string,
         nameCancel:PropTypes.string,
         onOkClick:PropTypes.func,
         onCancelClick:PropTypes.func,
     };
-        
+
+    static defaultProps = {
+        cardWidth:'sx',
+    }
+
     /** Defines the component visualization.
     * @returns JSX syntax element */
     render(){
         
         const jsx_component = (
-                <Dialog open={this.props.open} scroll='paper' 
+                <Dialog open={this.props.open} scroll='paper' fullWidth maxWidth={this.props.cardWidth}
                     TransitionComponent={down_animation} PaperProps={{sx:{alignSelf:'flex-start'}}}>
                     <DialogTitle variant='h5' align='left' color='text.secondary'>
                         {this.props.title}
                     </DialogTitle>
                     <DialogContent>
-                        <FormControl sx={{margin:'0.5rem', maxWidth:'25rem'}} fullWidth>
+                        <FormControl sx={{margin:'0.5rem'}} fullWidth>
                             {this.props.children}
                         </FormControl>
                     </DialogContent>
-                    <DialogActions>
+                    <DialogActions sx={{margin:'0.5rem'}}>
                         <Button variant='text' size='medium' color='primary'
                             onClick={this.props.onOkClick}>
                             {this.props.nameOk}
