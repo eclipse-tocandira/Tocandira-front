22643046444bb73d9c03f3823ca9735b5b9928b0
Pedro Ricardo C. Souza <pricardo@aimirimsti.com.br>
2023-11-09 12:22:35 -0300

    fix(DataTable): Enhance the Mui Data Grid configuration and solve infinite re-render on Chrome browser

--- src/component/DataTable/DataTable.js
+++ src/component/DataTable/DataTable.js
@@ -25,7 +25,11 @@ class DataTable extends React.PureComponent {
      
     /** Defines the component property types */
     static propTypes = {
-        headers:PropTypes.any,//.arrayOf(PropTypes.object),
+        sx: PropTypes.object,
+        show_empty: PropTypes.bool,
+        headers:PropTypes.any,
+        header_height:PropTypes.number,
+        row_height:PropTypes.number,
         content_rows:PropTypes.array,
         selected_row:PropTypes.object,
         with_checkbox: PropTypes.bool,
@@ -38,6 +42,7 @@ class DataTable extends React.PureComponent {
     };
 
     static defaultProps={
+        show_empty: false,
         with_checkbox: true,
         with_action_items: true,
         with_pagination: true,
@@ -66,7 +71,7 @@ class DataTable extends React.PureComponent {
 
         let action_items = null;
         if (this.props.with_action_items) {
-            action_items = <Box position='absolute' bottom='0.8rem' left='0.5rem'> 
+            action_items = <Box position='absolute' bottom='0.7rem' left='0.5rem'> 
                 <CardActionItems
                 enable_del_edit={this.props.selected_row.name===null}
                 onNewClick={this.props.onNewClick}
@@ -75,23 +80,28 @@ class DataTable extends React.PureComponent {
             </Box>;
         }
 
+        let table = null
+        if (this.props.content_rows.length>0 | this.props.show_empty) {
+            table = <DataGrid sx={{...remove_ugly_features, ...this.props.sx}}
+                rowHeight={this.props.row_height}
+                columnHeaderHeight={this.props.header_height}
+                columns={this.props.headers}
+                initialState={{pagination:{paginationModel:{pageSize:3}}}}
+                hideFooterPagination={!this.props.with_pagination}
+                checkboxSelection={this.props.with_checkbox}
+                pageSizeOptions={[3, 6, 9, 12]}
+                getRowId={(row) => row.name}
+                rows={this.props.content_rows}
+                hideFooterSelectedRowCount={true}
+                hideFooter={!this.props.with_pagination && !this.props.with_action_items}
+                onCellClick={this.props.onRowClick}
+                disableRowSelectionOnClick
+                onRowSelectionModelChange={this.handleSingleRowSelection}/>
+        }
+
         const jsx_component = (
-            <Box position='relative'>
-                <DataGrid sx={remove_ugly_features}
-                    rowHeight={35}
-                    columnHeaderHeight={40}
-                    columns={this.props.headers}
-                    initialState={{pagination:{paginationModel:{pageSize:3}}}}
-                    hideFooterPagination={!this.props.with_pagination}
-                    checkboxSelection={this.props.with_checkbox}
-                    pageSizeOptions={[3, 6, 9, 12]}
-                    getRowId={(row) => row.name}
-                    rows={this.props.content_rows}
-                    hideFooterSelectedRowCount={true}
-                    hideFooter={!this.props.with_pagination && !this.props.with_action_items}
-                    onCellClick={this.props.onRowClick}
-                    disableRowSelectionOnClick
-                    onRowSelectionModelChange={this.handleSingleRowSelection}/>
+            <Box position='relative' minHeight='3rem'>
+                {table}
                 {action_items}
             </Box>
         );
